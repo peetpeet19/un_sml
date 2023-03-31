9d8fb3549a9b9cf23c6f91d5389ace97e149994f
พีท อ่อนทอง <keypeet@gmail.com>
2023-03-31 12:31:04 +0700

    Create รวมมิตร.py

---
+++ รวมมิตร.py
@@ -0,0 +1,16 @@
+
+
+#Data
+
+#หง
+#https://drive.google.com/drive/folders/1Wk8KypsIy3TBBOmVhmCPkA0DiiCGzW1V?usp=sharing
+
+#ปาย
+#https://drive.google.com/drive/folders/1_V_UXlW3MTb9l_TzumvCG_d3eCe2FLNk
+
+#บอล
+#https://drive.google.com/drive/folders/1VfwB045lMizQxHY2URff_IbftZUIXJ4a?usp=sharing
+
+#ว้า
+#https://drive.google.com/drive/folders/1KY9HVfBj10xsKjY0tBHQXoG3pLrFOz0m?usp=sharing
+
